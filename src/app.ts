6b37e43906b5a3e24d2182b2dbd0ba373c13d8e2
mahi1212 <mahi121.mr@gmail.com>
2023-06-01 19:15:25 +0600

    added prettier and eslint

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 // const express = require('express')
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express()
+const app: Application = express()
 
-app.use(cors())
+        app.use(cors())
 // parser
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World successfull!')
+  res.send('Hello World successfull!')
 })
 
-export default app;
+export default app
