cc4a7ee6f164a9560e3272583c274a0b09b363fb
mahi1212 <mahi121.mr@gmail.com>
2023-06-03 13:35:31 +0600

    added interface and model

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-        app.use(cors())
+app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
