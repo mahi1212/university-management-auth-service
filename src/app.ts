cccd3d25a932c9e343f800f5a11e2053385a71d5
mahi1212 <mahi121.mr@gmail.com>
2023-07-04 22:56:26 +0600

    added academic semester create api

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application } from 'express'
 import cors from 'cors'
 import { userRoutes } from './app/modules/user/user.route'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import { AcademicSemesterRoute } from './app/modules/academicSemester/academicSemester.route'
 
 const app: Application = express()
 
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // application routes
 app.use('/api/v1/users/', userRoutes)
-
+app.use('/api/v1/academic-semesters/', AcademicSemesterRoute)
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
 //     //   throw new ApiError(400, 'Bad Request')
 //     throw new Error('Something went wrong')
